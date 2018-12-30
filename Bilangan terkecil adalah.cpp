e9da30588eb3262497e2176765102be90fb9a5be
muhammadyusufalfaqih <45525489+muhammadyusufalfaqih@users.noreply.github.com>
2018-12-30 15:59:37 -0800

    Add files via upload

---
+++ Bilangan terkecil adalah.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <conio.h>
+int minimum (int x,int y, int z);
+int main (void)
+{
+    int x,y,z,min;
+    printf("masukan bilangan 1 : ");scanf("%d",&x);
+    printf("masukan bilangan 2 : ");scanf("%d",&y);
+    printf("masukan bilangan 3 : ");scanf("%d",&z);
+    min=minimum (x,y,z);
+    printf("Bilangan terkecil adalah %d",min);
+    getch();
+}
+int minimum (int x, int y, int z)
+{
+    int min;
+    if (x>y) min=y;
+        else min=x;
+        if (z<min) min=z;
+        return (min);
+}
